c4870a537c1387a9ab69a0099fa794d6b80817d9
delphigu <delphigu@gmail.com>
2021-11-18 18:08:47 -0300

    resttemplate

--- hr-payroll/src/main/java/com/augusto/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/augusto/hrpayroll/services/PaymentService.java
@@ -1,14 +1,30 @@
 package com.augusto.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.augusto.hrpayroll.entities.Payment;
+import com.augusto.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
 	public Payment getPayment(long workerId,int days) {
-		return new Payment("Bob",200.00,10);
+		Map<String,String> variables= new HashMap<String,String>();
+		variables.put("id",String.valueOf(workerId));
+		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}",Worker.class,variables);
+		return new Payment(worker.getName(),worker.getDailyIncome(),days);
 	}
 
 }
